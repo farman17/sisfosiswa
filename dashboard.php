8a26d4c8daee54cab5b0d0c002cb1db6a88e325c
farman17 <69154746+farman17@users.noreply.github.com>
2021-01-06 07:33:44 +0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -24,7 +24,7 @@ $guru = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT COUNT(guru_pengampu) A
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title><?= $_SESSION['app_name'] ?> - Dashboard</title>
+  <title><?= $_SESSION['app_name'] ?> - Halaman Dashboard</title>
   <link href="_template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="_template/css/sb-admin-2.min.css" rel="stylesheet">
